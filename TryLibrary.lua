12652e1376ff605b3a37259f41a9b5c7696d05eb
Validark <NarrevRBX@gmail.com>
2017-10-08 19:05:41 -0600

    Update TryLibrary.lua

--- TryLibrary.lua
+++ TryLibrary.lua
@@ -1,94 +1,91 @@
--- A library for controlling the flow of error-prone, interdependent functions
+
+-- An asynchronous pcall-wrapper for interdependent error-prone functions
 -- @author Validark
 -- @readme https://github.com/F3XTeam/RBX-Try-Library/blob/master/README.md
 
-local function Package(self, Position, Success, Error, ...)
+local function Continue(self, Position, HistoryCount, Success, Error, ...)
 	if Success then
-		-- Don't enter `Then` from a `Catch`
-		if self[Position] ~= 2 then
-			for Position = Position + 3, self.Count, 3 do
-				-- Enter next `Then` function
-				if self[Position] == 1 then
-					self.LastArguments = {Error, ...}
-					self.Traceback = self.Traceback .. self[Position + 2]
-					return Package(self, Position, pcall(self[Position + 1], Error, ...)) -- Call next Then with arguments
-				end
+		for Position = Position + 3, self.Count, 3 do
+			-- Enter next `Then` function
+			if self[Position] == 1 then
+				self.LastArguments = {Error, ...}
+				self.History[HistoryCount + 1] = Position
+				return Continue(self, Position, HistoryCount + 1, pcall(self[Position + 1], Error, ...)) -- Call next Then with arguments
 			end
 		end
 	else
-		self.ErrorPosition = Position
 		for Position = Position + 3, self.Count, 3 do
 			-- Enter next `Catch` function
 			if self[Position] == 2 then
 				-- Get arguments and predicate count
 				local Arguments = self[Position + 1]
 				local PredicateCount = #Arguments - 1
+				local Caught = PredicateCount == 0
 
-				-- Handle any error if no predicates specified
-				if PredicateCount == 0 then
-					self.Traceback = self.Traceback .. self[Position + 2]
-					return Package(self, Position, pcall(Arguments[PredicateCount + 1], Error, self.Traceback .. "Stack End", self))
-
-				-- Handle matching error if predicates specified
-				elseif type(Error) == 'string' then
+				-- Handle matching error if strings were passed into Catch
+				if not Caught and type(Error) == "string" then
 					for Predicate = 1, PredicateCount do
 						if Error:match(Arguments[Predicate]) then
-							self.Traceback = self.Traceback .. self[Position + 2]
-							return Package(self, Position, pcall(Arguments[PredicateCount + 1], Error, self.Traceback .. "Stack End", self))
+							Caught = true
+							break
 						end
 					end
+				end
+				
+				if Caught then
+					local History = self.History
+					HistoryCount = HistoryCount + 1					
+					History[HistoryCount] = Position
+
+					-- Compile Traceback
+					local Traceback = "Stack Begin\n"
+					for a = 1, HistoryCount do
+						local Position = History[a]
+						local Type = self[Position]
+						local Message = (
+							Type == 0 and self[Position + 2]:match("Try.-[\n\r]([^\n\r]+)") .. " - upvalue Try\n" or
+							Type == 1 and self[Position + 2]:match("%- method Then[\n\r]([^\n\r]+)") .. " - method Then\n" or
+							self[Position + 2]:match("%- method Catch[\n\r]([^\n\r]+)") .. " - method Catch\n"
+						)
+						if Message then
+							Traceback = Traceback .. Message
+						end						
+					end
+					return Continue(self, Position, HistoryCount, pcall(Arguments[PredicateCount + 1], Error, Traceback .. "Stack End", self))
 				end
 			end
 		end
 	end
-	return {self, Position, Success, Error, ...}
+
+	-- This is what we are reduced to because Roblox refuses to fix coroutine.yield
+	-- https://devforum.roblox.com/t/wait-breaks-coroutine-yield/52881
+	self.Data = {Position, HistoryCount, Success, Error, ...}
+
+	-- Don't resolve thread if `Retry` was initiated
+	if self.PreviousRetryCount == self.RetryCount then
+		self.Resolved, self.RetryCount = true
+	else
+		self.PreviousRetryCount = self.RetryCount
+	end
 end
 
--- Define default Attempt properties
-local Attempt = {}
-Attempt.Count = 0
-Attempt.RetryCount = 0
+local function Start(self, Position, HistoryCount, ...)
+	Continue(self, Position, HistoryCount, pcall(self[Position + 1], ...))
+end
+
+local Attempt = {
+	[0] = 0;
+	Count = 2;
+	RetryCount = 0;
+	HistoryCount = 1;
+	PreviousRetryCount = 0;
+}
+
 Attempt.__index = Attempt
-
-local function Try(Function, ...)
-	--- Calls Function with (...) in protected mode, and returns chainable Attempt
-	-- @returns Attempt Object
-
-	local Bindable = Instance.new("BindableEvent")
-
-	local self = setmetatable({
-		[-1] = Function;
-		Bindable = Bindable;
-		Traceback = "Stack Begin\n" .. debug.traceback():match("Try[\n\r]([^\n\r]+)") .. " - upvalue Try\n";
-		LastArguments = {...};
-	}, Attempt)
-
-	local PreviousRetryCount, Results = 0
-
-	Bindable.Event:Connect(function(ErrorPosition)
-		-- Resume Thread and Cache results
-		if ErrorPosition then
-			Results = Package(self, ErrorPosition, pcall(self[ErrorPosition + 1], unpack(self.LastArguments)))
-		else
-			Results = Package(unpack(Results))
-		end
-
-		-- Don't resolve thread if `Retry` was initiated
-		if PreviousRetryCount == self.RetryCount then
-			self.Resolved, self.RetryCount = true
-		else
-			PreviousRetryCount = self.RetryCount
-		end
-	end)
-
-	Bindable:Fire(-2)
-
-	return self
-end
 
 function Attempt:__tostring()
 	if not self.Id then
-		self.Id = tostring{}:gsub('table', 'Attempt')
+		self.Id = tostring{}:gsub("table", "attempt")
 	end
 	return self.Id
 end
@@ -96,30 +93,22 @@ end
 function Attempt:Then(Function)
 	local Count = self.Count + 3
 	self.Count = Count
-	self[Count - 2] = 1
-	self[Count - 1] = Function
-	self[Count] = debug.traceback():match("%- method Then[\n\r]([^\n\r]+)") .. " - method Then\n"
-
+	self[Count - 2], self[Count - 1], self[Count] = 1, Function, debug.traceback()
 	if self.Resolved then
 		self.Resolved = false
-		self.Bindable:Fire()
+		coroutine.resume(coroutine.create(Continue), self, unpack(self.Data))
 	end
-
 	return self
 end
 
 function Attempt:Catch(...)
 	local Count = self.Count + 3
-	self.Count = Count
-	self[Count - 2] = 2
-	self[Count - 1] = {...}
-	self[Count] = debug.traceback():match("%- method Catch[\n\r]([^\n\r]+)") .. " - method Catch\n"
-
-	if self.Resolved and self.ErrorPosition then
+	self.Count = Count	
+	self[Count - 2], self[Count - 1], self[Count] = 2, {...}, debug.traceback()
+	if self.Resolved then
 		self.Resolved = false
-		self.Bindable:Fire()
+		coroutine.resume(coroutine.create(Continue), self, unpack(self.Data))
 	end
-
 	return self
 end
 
@@ -130,7 +119,28 @@ end
 
 function Attempt:Retry()
 	self.RetryCount = self.RetryCount + 1
-	self.Bindable:Fire(self.ErrorPosition)
+	local HistoryCount = #self.History
+	for a = HistoryCount, 1, -1 do
+		-- Find the last `Then` function and start it back up
+		local ErrorPosition = self.History[a]
+		if self[ErrorPosition] == 1 then
+			coroutine.resume(coroutine.create(Start), self, ErrorPosition, HistoryCount, unpack(self.LastArguments))
+			return self
+		end
+	end
+	error("[Try] Attempt has nothing to `Retry`")
+end
+
+local function Try(Function, ...)
+	--- Calls Function with (...) in protected mode, and returns chainable Attempt
+	-- @returns Attempt Object
+
+	local self = setmetatable({
+		Function, debug.traceback();
+		History = {0};
+		LastArguments = {...};
+	}, Attempt)
+	coroutine.resume(coroutine.create(Start), self, 0, 1, ...)
 	return self
 end
 
